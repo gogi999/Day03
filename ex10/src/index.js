a10443005dc17b7c04f215ba3b0e64319aa27732
gogi999 <g.petrovic9@gmail>
2021-03-10 16:33:05 +0100

    Added files in ex10/src folder

---
+++ ex10/src/index.js
@@ -0,0 +1,10 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    var product = numOne * numTwo;
+    // Only change code above this line
+    return product;   
+}
+
+console.log(main(425, 10)); // Change this line
+
+module.exports = main;

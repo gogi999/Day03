382e948d7d3fb9bf077c1dc183bbafd53aab9727
gogi999 <g.petrovic9@gmail>
2021-03-10 09:55:17 +0100

    Added files in ex02/src folder

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    // Only change code below this line
+    // Example of a single line JavaScript comment!
+    /* 
+    Example of a multi line JavaScript comment! First line...
+    Second line...
+    Third line...
+    ...
+    */
+    // Only change code above this line
+    
+}
+
+console.log(main());
+
+module.exports = main;

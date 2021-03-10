178578a597b9181ca7fc8c9f6a50177589f424c0
gogi999 <g.petrovic9@gmail>
2021-03-10 11:50:04 +0100

    Added files in ex07/src folder

---
+++ ex07/src/index.js
@@ -0,0 +1,23 @@
+function main() {
+    // Only change code below this line
+    // Varable declarations
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+
+    // Varable assignment
+    studlyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseOver = 9000;
+    
+    // Only change code above this line
+    return {
+        studlyCapVar,
+        properCamelCase,
+        titleCaseOver
+    }   
+}
+
+console.log(main());
+
+module.exports = main;
